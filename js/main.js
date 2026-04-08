bf894fb4cad82eb1a869c1d73514e7604f8e2938
Amospikins <sanmco2013@gmail.com>
2026-04-08 22:32:23 +0100

    third commit

--- js/main.js
+++ js/main.js
@@ -3,17 +3,30 @@ const pads = document.querySelectorAll(".pad");
 
 for (let i = 0; i < pads.length; i++) {
     pads[i].addEventListener("click", function () {
-        const sound_path = pads[i].getAttribute("data-sound");
-        new Audio(sound_path).play();
-
-
+        // const sound_path = pads[i].getAttribute("data-sound");
+        // const audio = new Audio(sound_path);
+        // audio.currentTime = 0;
+        // audio.play();
 
         // pads[i].classList.add("active");
         // setTimeout(function () {
         //     pads[i].classList.remove("active");
         // }, 100);
+
+
+        playSound(pads[i]);
     });
 }
 
+function playSound(pad) {
+    const sound_path = pad.getAttribute("data-sound");
+    const audio = new Audio(sound_path);
+    audio.currentTime = 0;
+    audio.play();
+    pad.classList.add("active");
+    setTimeout(function () {
+        pad.classList.remove("active");
+    }, 100);
+}
 
 // new Audio(../assets/boom.wav).play();
